cedb11be8d41dc5adbf274b1bd13c92c31a8a6ed
Durgesh Tarkar <142762097+durgeshtarkar@users.noreply.github.com>
2026-01-31 16:25:43 +0530

    Time: 1 ms (99.63%) | Memory: 44 MB (22.17%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,41 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-                if (s == null || s.length() == 0) return 0;
-
-        int i = 0, n = s.length();
-        // 1. Skip leading whitespace
-        while (i < n && s.charAt(i) == ' ') {
+        if(s.length() == 0){
+            return 0;
+        }
+        int i = 0;
+        while(i < s.length() && s.charAt(i) == ' '){
             i++;
         }
-
-        // If string is empty after trimming
-        if (i == n) return 0;
-
-        // 2. Handle sign
+        if (i == s.length()) return 0;
         int sign = 1;
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
+        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
+            sign = (s.charAt(i)=='-')? -1 : 1;
             i++;
         }
-
-        // 3. Convert digits
-        long result = 0; // use long to detect overflow
-        while (i < n && Character.isDigit(s.charAt(i))) {
+        long res = 0;
+        while(i < s.length() && Character.isDigit(s.charAt(i))){
             int digit = s.charAt(i) - '0';
-            result = result * 10 + digit;
-
-            // 4. Clamp to 32-bit signed integer range
-            if (sign == 1 && result > Integer.MAX_VALUE) {
+            res = 10*res + digit;
+            if(sign == 1 && res > Integer.MAX_VALUE){
                 return Integer.MAX_VALUE;
             }
-            if (sign == -1 && -result < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
+            if(sign == -1 && -res < Integer.MIN_VALUE){
+                return  Integer.MIN_VALUE;
             }
-
             i++;
-         }
-
-        return (int) result * sign;
-
+        }
+        
+        return (int)(sign*res);  
     }
 }
